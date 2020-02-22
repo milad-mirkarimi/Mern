50f3e7834dd8b549b334954313f0db81cb1fa044
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-02-22 20:12:50 +1300

    changes

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+
+router.get('/', (req, res) => {
+  console.log(req.body);
+  res.send('user route');
+});
+
+module.exports = router;
