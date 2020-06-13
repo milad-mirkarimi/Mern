c6368e274137a435f4c79d39a7e179c7efac13fb
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-06-13 19:21:15 +1200

    Add Login and register FE

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
+    const { email, password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log('successful');
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into your account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" required value={email} onChange={e => onChange(e)} />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        required
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+        </Fragment>
+    )
+};
 export default Login;
