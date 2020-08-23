7a2e68424329c77139e8980b4521b3421f1e2b71
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-08-23 22:58:37 +1200

    add dashboard basics

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = (props) => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+    useEffect(() => {
+        props.getCurrentProfile()
+    }, []);
+    return props.profile.loading && props.profile.profile === null ? <div></div> : <Fragment>
+        <h1>Welcome {props.profile.profile.user && props.profile.profile.user.name}</h1>
+    </Fragment>
 }
 
 Dashboard.prototypes = {
-
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
 }
 
-export default Dashboard
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
