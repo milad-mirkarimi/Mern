1c10a5f1c126beebe282f7b7e470c18de700ad9c
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-10-12 08:24:07 +1300

    add delete experience

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { setAlert } from './alert';
 
 import {
+    CLEAR_PROFILE,
     GET_PROFILE,
     PROFILE_ERROR,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    DELETE_ACCOUNT
 } from './types';
 
 // GET current users profile
@@ -123,5 +125,64 @@ export const addEducation = (formData, history) => async dispatch => {
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
         })
+    }
+}
+
+// DELETE experience
+
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert('Experience Removed!', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// DELETE education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert('Education Removed!', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//DELETE account and profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        try {
+            const res = await axios.delete(`/api/profile`);
+
+            dispatch({
+                type: CLEAR_PROFILE
+            });
+            dispatch({
+                type: DELETE_ACCOUNT
+            });
+            dispatch(setAlert('Your account has been deleted!'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            })
+        }
     }
 }
