d45ea4f1f2f93250db9ed47951c9d468cce6ed95
milad-mirkarimi <themiladmirkarimi@gmail.com>
2020-10-12 22:01:45 +1300

    remove user post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const config = require('config')
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -142,8 +143,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
     try {
-        // TODO: remove users posts
-
+        // Remove User posts
+        await Post.deleteMany({ user: req.user.id });
         // Remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
         // Remove user
